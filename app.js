9c7ee61febc82b3ac02c300e85a0941a4429727b
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-24 14:40:19 +0530

    request object setting up routes: request.url

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  console.log('user hit the server')
+  const url = request.url
   // Let's add header info and status
-  response.writeHead(200, { 'content-type': 'text/html' })
-  response.write('<h1>Welcome to the World of Node</h1>')
-  response.end()
+
+  if (url === '/') {
+    response.writeHead(200, { 'content-type': 'text/html' })
+    response.write('<h1>Homepage!</h1>')
+    response.end()
+  } else if (url === '/about') {
+    response.writeHead(200, { 'content-type': 'text/html' })
+    response.write('<h1>About Page!</h1>')
+    response.end()
+  } else {
+    response.writeHead(404, { 'content-type': 'text/html' })
+    response.write('<h1>Found Not Found!</h1>')
+    response.end()
+  }
 })
 
 server.listen(5000)
