00ff9b30a56e8c1231249d21144b54dad2cc8e8e
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-24 18:32:38 +0530

    Quickly getting starting with Express

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
-
 const app = express()
-
 const port = 5000
+
+app.get('/', (request, response) => {
+  response.send('<h1>Home Page</h1>')
+})
+
+app.get('/about', (request, response) => {
+  response.status(200).send('<h1>About Page</h1>')
+})
+
+app.all('*', (request, response) => {
+  response.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(port, () => {
   console.log('Server is listening on port ' + port)
