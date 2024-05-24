1415c7216bca731299eba7507081c3c8537f3238
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-24 16:21:12 +0530

    Setting up routes and handling requests for  internal links of html file as well

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+// const homepage = readFileSync('./index.html')
+const homepage = readFileSync('./navbar-app/index.html')
+
+/** --- The server needs to explicitly  handle resources within the index.html, like style.css, logo.svg, browser-app.js ---*/
+
+const styleSheet = readFileSync('./navbar-app/styles.css')
+const logo = readFileSync('./navbar-app/logo.svg')
+const homeJS = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((request, response) => {
   const url = request.url
@@ -6,7 +16,19 @@ const server = http.createServer((request, response) => {
 
   if (url === '/') {
     response.writeHead(200, { 'content-type': 'text/html' })
-    response.write('<h1>Homepage!</h1>')
+    response.write(homepage)
+    response.end()
+  } else if (url === '/styles.css') {
+    response.writeHead(200, { 'content-type': 'text/css' })
+    response.write(styleSheet)
+    response.end()
+  } else if (url === '/logo.svg') {
+    response.writeHead(200, { 'content-type': 'image/svg+xml' })
+    response.write(logo)
+    response.end()
+  } else if (url === '/browser-app.js') {
+    response.writeHead(200, { 'content-type': 'text/javascript' })
+    response.write(homeJS)
     response.end()
   } else if (url === '/about') {
     response.writeHead(200, { 'content-type': 'text/html' })
