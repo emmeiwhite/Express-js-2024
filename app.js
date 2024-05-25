da201e0db131734edf9c361e42bedbe54322d16a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-25 22:22:27 +0530

    Logger setup with middleware for each and every route

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1>')
 })
 
+app.get('/api/products', (req, res) => {
+  res.status(200).json([
+    { id: 1, name: 'iphone' },
+    { id: 2, name: 'macbook-pro' }
+  ])
+})
+
+app.get('/api/products/:productID', (req, res) => {
+  const id = req.params.productID
+  res.send(`<h1>The Product with id: ${id}</h1>`)
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on Port 5000')
 })
