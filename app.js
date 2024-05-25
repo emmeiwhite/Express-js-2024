d51ca050e4f7cfaf7b1a61937b5c162c43691a2c
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-25 22:18:38 +0530

    Middleware order matters,  added logger to each route

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const express = require('express')
 const app = express()
 
+const logger = require('./logger')
+
 // req => middle => res
-// middleware sits in b/w req and res so to say and middleware is everywhere in nodejs
 
-/** Say we want to add logger information of each request */
+app.use(logger)
 
-const logger = (req, res, next) => {
-  const method = req.method
-  const url = req.url
-  const time = new Date().getFullYear()
-
-  console.log(method, url, time)
-
-  next()
-}
-
-app.get('/', logger, (req, res) => {
+app.get('/', (req, res) => {
   res.send('<h1>Homepage</h1>')
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1>')
 })
 
