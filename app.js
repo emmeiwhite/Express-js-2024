b837d9fb6e5bdf2fff86254bdf57839d3748caeb
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-25 22:11:42 +0530

    Setting up my first middleware after 2020

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+// req => middle => res
+// middleware sits in b/w req and res so to say and middleware is everywhere in nodejs
+
+/** Say we want to add logger information of each request */
+
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+
+  console.log(method, url, time)
+
+  next()
+}
+
+app.get('/', logger, (req, res) => {
   res.send('<h1>Homepage</h1>')
 })
+
+app.get('/about', logger, (req, res) => {
+  res.send('<h1>About Page</h1>')
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on Port 5000')
 })
