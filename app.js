9d2eb9c180b86e93575ed4f984df907badd89195
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-25 16:32:37 +0530

    Sending API  from the server

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
-
-// setup static & middleware
-app.use(express.static('./public'))
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// dump index.html in a public folder as well & server will automatically serve it. So to make a static site in node, put every static assets in the public folder
-// })
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+const jsonData = [
+  {
+    name: 'John'
+  },
+  {
+    name: 'Emmei'
+  }
+]
+app.get('/', (req, res) => {
+  res.json(jsonData)
 })
 
 app.listen(5000, () => {
-  console.log('server is listening to port 5000')
+  console.log('Server is listening on port 5000')
 })
