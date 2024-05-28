c7920386ec106b5281575598acc5a2a79c4e4f65
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-28 12:50:51 +0530

    post request with forms

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const { people } = require('./data')
 // let's use middleware from express | Create static site
 app.use(express.static('./methods-public'))
 
+// In order to access the form data, we'll use middleware provided by express
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
+})
+
+app.post('/login', (req, res) => {
+  const { username } = req.body
+
+  if (username) {
+    res.status(200).send(`<h1>Welcome :${username} 🌅</h1>`)
+  } else {
+    res.status(401).send(`<h1>Please provide Credentials</h1>`)
+  }
 })
 
 app.listen(5000, () => {
