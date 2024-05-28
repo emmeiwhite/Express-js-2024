a7150ef861a2ca955aed6e6c602999e38151c02d
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-28 11:11:44 +0530

    Mimicked api - sending back people json data api

--- app.js
+++ app.js
@@ -1,53 +1,12 @@
 const express = require('express')
 const app = express()
 
-const morgan = require('morgan') // third party middleware logger
+const { people } = require('./data')
 
-const logger = require('./logger')
-const authorize = require('./authorize')
-
-app.use(morgan('tiny')) // set on all routes to get basic data about request
-
-// req => middle => res
-// applying logger to all the routes starting with /api
-// app.use('./api', logger)
-
-/** using multiple middlewares in an array */
-// app.use([logger, authorize])
-
-app.get('/', (req, res) => {
-  res.send('<h1>Homepage</h1>')
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>')
-})
-
-app.get('/api/products', (req, res) => {
-  res.status(200).json([
-    { id: 1, name: 'iphone' },
-    { id: 2, name: 'macbook-pro' }
-  ])
-})
-
-// Providing the middleware only for this route so that it is not applied for all the routes - now the app runs smoothly
-app.get('/api/customers', [logger, authorize], (req, res) => {
-  // we can now access req.user
-  const { name, id } = req.user
-  res.status(200).json([
-    {
-      id,
-      user: name,
-      authorized: true
-    }
-  ])
-})
-
-app.get('/api/products/:productID', (req, res) => {
-  const id = req.params.productID
-  res.send(`<h1>The Product with id: ${id}</h1>`)
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on Port 5000')
+  console.log('Listening to Port 5000')
 })
