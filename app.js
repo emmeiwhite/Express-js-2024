5abd3db7a576b400a36cbc700724a2ee58679a77
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-07 14:09:02 +0530

    PUT & DELETE Methods covered

--- app.js
+++ app.js
@@ -47,6 +47,42 @@ app.post('/api/people', (req, res) => {
   res.status(200).json({ success: true, person: name })
 })
 
+/** --- PUT --- */
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const person = people.find(person => person.id === Number(id))
+
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with the id ${id}` })
+  }
+
+  const newPeople = people.map(person => {
+    if (person.id === Number(id)) {
+      return { ...person, name }
+    }
+    return person
+  })
+
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+/** --- DELETE --- */
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params
+
+  const person = people.find(person => person.id === Number(id))
+
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with the id ${id}` })
+  }
+
+  const newPeople = people.filter(person => person.id !== Number(id))
+
+  res.status(200).json({ success: true, data: newPeople })
+})
+
 app.listen(5000, () => {
   console.log('Listening to Port 5000')
 })
